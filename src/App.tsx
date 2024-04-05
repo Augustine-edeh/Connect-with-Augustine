ac421ab1c2c69ac1fdab98e1effbeabcf7888108
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-05 15:43:13 +0100

    Basic layout line-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="grid place-items-center h-screen bg-off_black">
+      <main className="bg-dark_grey text-white">
+        <p>Avatar</p>
+
+        <p>Name</p>
+
+        <p>Location</p>
+
+        <p>Description</p>
+
+        <div>links</div>
+      </main>
+    </div>
+  );
 };
 export default App;
