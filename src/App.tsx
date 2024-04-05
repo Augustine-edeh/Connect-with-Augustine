c138ad1f4e51b4c8f5166fdc5f1d5f826bce19de
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-05 15:57:34 +0100

    add Avatar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Avatar from "./Avatar";
+
 const App = () => {
   return (
     <div className="grid place-items-center h-screen bg-off_black">
-      <main className="bg-dark_grey text-white">
-        <p>Avatar</p>
-
+      <main className="grid place-items-center bg-dark_grey text-white p-5 rounded-xl">
+        <Avatar />
         <p>Name</p>
 
         <p>Location</p>
---
+++ src/Avatar.tsx
@@ -0,0 +1,13 @@
+const Avatar = () => {
+  return (
+    <picture className="bg-white p-1 rounded-full mb-8">
+      <img
+        src={"Augustine.jpeg"}
+        alt="Augstine-img"
+        className="h-48 rounded-full"
+      />
+    </picture>
+  );
+};
+
+export default Avatar;
