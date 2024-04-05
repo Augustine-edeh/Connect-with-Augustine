5fd5c691aa61474432e4185834ab991cf0f3f84e
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-05 15:28:41 +0100

    complete initialization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      green: "hsl(75,94%,57%)",
+      white: "hsl(0,0%,100%)",
+      grey: "hsl(0,0%,20%)",
+      dark_grey: "hsl(0,0%,12%)",
+      off_black: "hsl(0,0%,8%)",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
