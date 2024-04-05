bffb73447404fdc4e447c7f227e45265b8e97df6
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-05 19:34:28 +0100

    adjust styles for Location

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,5 +1,5 @@
 const Location = () => {
-  return <div className="text-green font-bold">Lagos, Nigeria</div>;
+  return <div className="text-green font-bold mb-6">Lagos, Nigeria</div>;
 };
 
 export default Location;
