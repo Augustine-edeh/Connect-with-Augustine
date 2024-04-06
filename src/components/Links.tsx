69f7167518f7879518b08993fb92d5c0d877bd46
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-06 01:30:02 +0100

    add Social-Link json file

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,45 +1,34 @@
+import { useState, useEffect } from "react";
+
 const Links = () => {
-  const socialLinks = [
-    {
-      name: "Twitter",
-      url: "https://twitter.com/exampleuser",
-      icon: "twitter-icon.svg",
-    },
-    {
-      name: "Instagram",
-      url: "https://instagram.com/exampleuser",
-      icon: "instagram-icon.svg",
-    },
-    {
-      name: "Facebook",
-      url: "https://facebook.com/exampleuser",
-      icon: "facebook-icon.svg",
-    },
-    {
-      name: "LinkedIn",
-      url: "https://linkedin.com/in/exampleuser",
-      icon: "linkedin-icon.svg",
-    },
-    {
-      name: "Frontend Mentor",
-      url: "https://www.frontendmentor.io/profile/exampleuser",
-      icon: "frontendmentor-icon.svg",
-    },
-    {
-      name: "Personal Portfolio",
-      url: "https://exampleuser.com",
-      icon: "portfolio-icon.svg",
-    },
-  ];
+  const [socialLinks, setSocialLinks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("../../public/data/socialLinks.json");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setSocialLinks(data);
+      } catch (error) {
+        console.error("Error fetching social-links data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <ul className="w-full">
-      {socialLinks.map((social) => (
+      {socialLinks.map(({ name, url, key }) => (
         <a
-          href={social.url}
+          href={url}
+          key={key}
           className="block outline-none active:text-off_black hover:text-off_black hover:bg-green focus:bg-green focus:text-off_black bg-grey text-center rounded-xl py-3.5 my-5"
         >
-          {social.name}
+          {name}
         </a>
       ))}
     </ul>
