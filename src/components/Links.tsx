0fe1dd8892b56887c0c435cfdd4ab1b8f4978956
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-04-07 05:34:21 +0100

    improve UI semantics

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -4,13 +4,12 @@ const Links = () => {
   return (
     <ul className="w-full">
       {SocialLinks.map(({ name, url, key }) => (
-        <a
-          href={url}
+        <li
           key={key}
-          className="block outline-none font-InterSemiBold active:text-off_black hover:text-off_black hover:bg-green focus:bg-green focus:text-off_black bg-grey text-center rounded-xl py-3.5 my-5"
+          className="outline-none font-InterSemiBold active:text-off_black hover:text-off_black hover:bg-green focus:bg-green focus:text-off_black bg-grey text-center rounded-xl py-3.5 my-5"
         >
-          {name}
-        </a>
+          <a href={url}>{name}</a>
+        </li>
       ))}
     </ul>
   );
